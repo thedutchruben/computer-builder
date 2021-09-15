4ea80dc99a6886f90011d7a16a264b88587eec2f
Ruben de Roos <deroosr@gmail.com>
2021-09-15 20:14:01 +0200

    clear project
    clear project

--- pages/HomePage.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include 'templates/Header.html';
-?>
-
-<div id="base-text">
-    <h2 class="header">Welkom op pc builder site hier kan je je computer samenstellen</h2>
-    <p></p>
-</div>
--- src/framework/Execptions/TemplateNotFound.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace PcBuilder\Framework\Execptions;
-
-class TemplateNotFound extends \Exception
-{
-
-}
